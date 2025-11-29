06c759a338c3095faadae7926694a825b4208a11
supergodine <massuplas@gmail.com>
2025-11-29 10:40:29 -0300

    fix: tailwindcss, postcss, autoprefixer dependências e configs para build Vercel

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,7 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+// Updating to ensure compatibility with Vercel and Tailwind
 export default {
   plugins: {
     tailwindcss,
