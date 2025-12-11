7d23f780d5b3e097fdbe5ca630bc34347d26ee2b
supergodine <massuplas@gmail.com>
2025-12-10 21:32:25 -0300

    fix(mercadopago): correct Nest imports (remove HttpModule/HttpService)

--- backend/src/modules/mercadopago/mercadopago.module.ts
+++ backend/src/modules/mercadopago/mercadopago.module.ts
@@ -1,4 +1,4 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { PaymentsModule } from '../payments/payments.module';
